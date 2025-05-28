f02d578a83709e5a09b78bbf2a0b6f7d37d9a69f
Robson <robson.arakawa@gmail.com>
2025-05-28 03:49:48 -0300

    Adicionando testes e removendo o arquivo da pasta errada

--- Testes/01-testes.js
+++ Testes/01-testes.js
@@ -2,10 +2,12 @@
     describe -> abrupador de testes
     it -> implementação do teste
 */
+import {} from '../Conceitos/exibirNomeFormatado.js'
+import assert from 'node:assert'
 
 describe('Testes do projeto', () => {
 
-    it('Primeiro teste', () => {
-        
+    it('Deve exibir o nome do cachorro com letras maiúsculas', () => {
+        assert.strictEqual(exibirNomeFormatado('Pepi'), 'PEPI')
     })
 })
