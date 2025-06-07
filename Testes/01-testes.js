a9f3eda3d9e2d52ed18323e06ee0521e1b98e460
Robson <robson.arakawa@gmail.com>
2025-06-07 18:05:59 -0300

    Corrigindo arquivo de teste

--- Testes/01-testes.js
+++ Testes/01-testes.js
@@ -2,7 +2,7 @@
     describe -> abrupador de testes
     it -> implementação do teste
 */
-import {} from '../Conceitos/exibirNomeFormatado.js'
+import {exibirNomeFormatado} from '../Conceitos/exibirNomeFormatado.js'
 import assert from 'node:assert'
 
 describe('Testes do projeto', () => {
